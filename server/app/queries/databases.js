da5c661efb820d904185ecfa8f0dd8cf1e60f400
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-09 20:42:08 +0100

    add option to drop database

--- server/app/queries/databases.js
+++ server/app/queries/databases.js
@@ -21,8 +21,25 @@ module.exports = function (app) {
         conn.query('CREATE DATABASE ??', [req.body.database], function (err, results, fields) {
             if (err) {
                 res.status(500).json({ message: "Error when creating new database." });
-                throw err;
             }
+            res.status(200).json({ results });
+        });
+    });
+
+    app.put('/databases/:databaseName', function (req, res) {
+        if (req.params.databaseName === undefined) {
+            res.status(500).json({ message: "Database name not specified!" });
+            return;
+        }
+
+        let conn = connection.getConnection();
+
+        conn.query("DROP DATABASE ??", [req.params.databaseName], function (err, results, fields) {
+            if (err) {
+                res.status(500).json({ message: "An error has occurred when deleting a database"});
+                return;
+            }
+
             res.status(200).json({ results });
         });
     });
