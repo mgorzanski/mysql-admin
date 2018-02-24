311a66064a5e2c477a7adff1f6eb851a2af0e9dd
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-24 15:44:15 +0100

    update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import { faHome, faUsers, faDatabase, faSlidersH, faWindowMaximize, faSignOutAlt } from '@fortawesome/fontawesome-free-solid';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Nav extends React.Component {
     render() {
@@ -12,12 +12,12 @@ class Nav extends React.Component {
         return (
             <nav className="primary-nav">
                 <ul className="primary-nav__primary-menu">
-                    <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
-                    <li><Link to="/databases"><FontAwesomeIcon icon={faDatabase} /> Databases</Link></li>
-                    <li><Link to="/sql"><FontAwesomeIcon icon={faWindowMaximize} /> SQL</Link></li>
-                    <li><Link to="/users"><FontAwesomeIcon icon={faUsers} /> Users</Link></li>
-                    <li><Link to="/settings"><FontAwesomeIcon icon={faSlidersH} /> Settings</Link></li>
-                    <li><Link to="/logout" innerRef={refLogout}><FontAwesomeIcon icon={faSignOutAlt} /> Logout</Link></li>
+                    <li><NavLink exact activeClassName="primary-nav__item-active" to="/"><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faHome} /></div><div className="primary-nav__item-title">Home</div></NavLink></li>
+                    <li><NavLink activeClassName="primary-nav__item-active" to="/databases"><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faDatabase} /></div><div className="primary-nav__item-title">Databases</div></NavLink></li>
+                    <li><NavLink activeClassName="primary-nav__item-active" to="/sql"><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faWindowMaximize} /></div><div className="primary-nav__item-title">SQL</div></NavLink></li>
+                    <li><NavLink activeClassName="primary-nav__item-active" to="/users"><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faUsers} /></div><div className="primary-nav__item-title">Users</div></NavLink></li>
+                    <li><NavLink activeClassName="primary-nav__item-active" to="/settings"><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faSlidersH} /></div><div className="primary-nav__item-title">Settings</div></NavLink></li>
+                    <li><NavLink to="/logout" innerRef={refLogout}><div className="primary-nav__item-icon"><FontAwesomeIcon icon={faSignOutAlt} /></div><div className="primary-nav__item-title">Logout</div></NavLink></li>
                 </ul>
             </nav>
         );
